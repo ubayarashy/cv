4870c9140279cccc1ec3e0fefb8e6addb57e723b
ubayarashy <arashyubay@gmail.com>
2025-02-18 00:16:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ var projectSwiper = new Swiper('.project-container', {
         clickable: true,
     },
 });
+const swiper = new Swiper('.swiper-container', {
+    loop: true, // Menambahkan perulangan
+    slidesPerView: 1, // Jumlah slide yang ditampilkan
+    spaceBetween: 30, // Jarak antar slide
+    navigation: {
+        nextEl: '.swiper-button-next', // Tombol next
+        prevEl: '.swiper-button-prev', // Tombol prev
+    },
+    pagination: {
+        el: '.swiper-pagination', // Menampilkan pagination
+        clickable: true, // Membuat pagination bisa diklik
+    },
+    autoplay: {
+        delay: 2500, // Waktu otomatis pindah slide dalam milidetik
+    },
+});
